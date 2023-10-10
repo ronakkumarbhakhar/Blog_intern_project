31621ebff3bcc3165fef9dad912ff12e7e53ea5b
ronak <rk8875636542@gmail.com>
2023-10-10 18:49:17 +0530

    first commit

--- middleware/searchMiddleware.js
+++ middleware/searchMiddleware.js
@@ -1,28 +1,52 @@
 const axios=require('axios');
-const lodash=require('lodash');
+const _=require('lodash');
 
+//  getAnalyticData() function is passed to _.memoize() method for caching
+async function getAnalyticData(url){
+    let {data} = await axios.get(url, 
+    {
+        headers: {
+        'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6'
+        }
+    });
+    return data;
+}
+
+// searchQuery function is used in filterQuery() function to find if query is prsent in blog title or not
 let searchQuery=function(d,word){
-    if(lodash.includes(d.title.toLowerCase(),word.toLowerCase())){
+    if(_.includes(d.title.toLowerCase(),word.toLowerCase())){
         return true;
     }
     return false; 
 }
 
+// filterQuery function is used for filtering out all blogs containg query
+function filterQuery(data,search){
+    return _.filter(data,function(d){return searchQuery(d,search)});
+}
+
+// resolver function to be passed as resolver in _.memoize() method
+function resolver(...args) {
+    const now = new Date();
+    const key = Math.floor(now.getSeconds() / 10 ) * 10;
+    return key;
+  }
 
 exports.searchmiddleware=async function(req,res,next){
     try{
-        let {data} = await axios.get('https://intent-kit-16.hasura.app/api/rest/blogs', 
-        {
-            headers: {
-            'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6'
-            }
-        })
+        let getAnalyticDataMemoize=_.memoize(getAnalyticData,resolver); //memoization is done with help of _.memoize() method of lodash
+        let data= await getAnalyticDataMemoize('https://intent-kit-16.hasura.app/api/rest/blogs');
+
         data=data.blogs;
         let search=req.query.query;   
+        // memoization of filtered blogs
+        let filterQueryMemozie=_.memoize(filterQuery,resolver);
         // result object used for storing all analysis data
-        let result={};
-        result=lodash.filter(data,function(d){return searchQuery(d,search)}); // all blogs that contain specific word (word we are quering for) in title
-        req.result=result; // adding data to req 
+        let result={}; 
+        // all blogs that contain specific word (word we are quering for) in title
+        result=filterQueryMemozie(data,search) 
+        // adding data to req 
+        req.result=result; 
     }
     catch(error){
         let result={}
--- middleware/statMiddleware.js
+++ middleware/statMiddleware.js
@@ -1,35 +1,58 @@
 const axios=require('axios');
-const lodash=require('lodash');
+const _=require('lodash');
 
+
+
+//  getAnalyticData() function is passed to _.memoize() method for caching
+async function getAnalyticData(url){
+    let {data} = await axios.get(url, 
+    {
+        headers: {
+        'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6'
+        }
+    });
+    return data;
+} 
+
+// resolver function to be passed as resolver in _.memoize() method
+function resolver(...args) {
+    const now = new Date();
+    const key = Math.floor(now.getSeconds() / 10 ) * 10;
+    return key;
+  }
+
+// containsPrivacy function is used to check if blog contains privacy word in its title
 let containsPrivacy=function(d){
-    if(lodash.includes(d.title.toLowerCase(),"privacy")){
+    if(_.includes(d.title.toLowerCase(),"privacy")){
         return true;
     }
     return false; 
 }
 
-
+//  middleware function
 exports.statmiddleware=async function(req,res,next){
+    // try catch finally used for error handling
     try{
-        let {data} = await axios.get('https://intent-kit-16.hasura.app/api/rest/blogs', 
-        {
-            headers: {
-            'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6'
-            }
-        });
+        let getAnalyticDataMemoize=_.memoize(getAnalyticData,resolver); //memoization is done with help of _.memoize() method of lodash
+        let data= await getAnalyticDataMemoize('https://intent-kit-16.hasura.app/api/rest/blogs');
+
         data=data.blogs;
+
         // creating a key value pair named titleSize so that data can be sorted in asccending order of title length
-        lodash.forEach(data,function(obj){obj.titleSize=lodash.size(obj.title)})
+        _.forEach(data,function(obj){obj.titleSize=_.size(obj.title)})
+
         // sorting the collection
-        data=lodash.sortBy(data,function(d){return d.titleSize});
+        data=_.sortBy(data,function(d){return d.titleSize});
+
         // result object used for storing all analysis data
         let result={};
-        result.count=lodash.size(data); //total number of blog 
-        result.longestTitle=data[result.count-1]; // log with longest title
-        result.containsPrivacy=lodash.filter(data, containsPrivacy); // all blogs that contain Privacy or privacy in title
-        result.unique=lodash.uniqBy(data,'title');// all blogs with unique titles
 
-        lodash.forEach(data,function(obj){delete obj.titleSize}) // removing titleSize from all blogs as it is no longer required
+        result.count=_.size(data); //total number of blog 
+        result.longestTitle=data[result.count-1]; // log with longest title
+        result.containsPrivacy=_.filter(data, containsPrivacy); // all blogs that contain Privacy or privacy in title
+        result.unique=_.uniqBy(data,'title');// all blogs with unique titles
+
+        _.forEach(data,function(obj){delete obj.titleSize}) // removing titleSize from all blogs as it is no longer required
         req.result=result; // adding data to req
     }catch(error){
         let result={};
